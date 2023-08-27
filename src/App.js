9bdf6ca34d2678bf27b3a13da4376c28d332fbcf
Diana Dvoryak <dianadvoryak.w@gmail.com>
2023-08-27 18:26:07 +0200

    crate post

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useRef, useState} from "react";
+import './styles/App.css';
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [posts, setPosts] = useState([
+        {id: 1, title: 'Javascript1', body: 'Description1'},
+        {id: 2, title: 'Javascript2', body: 'Description2'},
+        {id: 3, title: 'Javascript3', body: 'Description3'},
+    ]);
+    const [post, setPost] = useState({title: '', body: ''})
+
+    const addNewPost = (e) => {
+        e.preventDefault()
+        setPosts([...posts, {...post, id: Date.now()}])
+        setPost({title: '', body: ''})
+    }
+
+    return (
+        <div className="App">
+            <form>
+                <MyInput
+                    value={post.title}
+                    onChange={e => setPost({...post, title: e.target.value})}
+                    type="text"
+                    placeholder="Название поста"
+                />
+                <MyInput
+                    value={post.body}
+                    onChange={e => setPost({...post, body: e.target.value})}
+                    type="text"
+                    placeholder="Описание поста"
+                />
+                <MyButton onClick={addNewPost}>Создать пост</MyButton>
+            </form>
+            <PostList posts={posts} title={'Список постов 1'}/>
+        </div>
+    );
 }
 
 export default App;
